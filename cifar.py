9a56fa9ee87c4af4db50062b578420484df2655b
Xiangfeng(Allen) Zhu <xzhu27@ucsc.edu>
2020-08-29 11:52:09 -0400

    Tweaks

--- cifar.py
+++ cifar.py
@@ -38,66 +38,71 @@ def main():
     else:
         model = se_resnet20(num_classes=10, reduction=args.reduction).to(device)
 
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    model.load_state_dict(torch.load('checkpoint/cifar_senet.pth'))
+    # for name, value in model.named_parameters():
+    #     # value.requires_grad = False
+    #     print('name: {0},\t grad: {1}'.format(name, value.requires_grad))
+    torch.save(model, 'checkpoint/cifar_senet_full.pth')
 
 
+    # criterion = nn.CrossEntropyLoss()
+    # optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-    for epoch in range(20):  # loop over the dataset multiple times
-        print(f"Start training epoch {epoch}")
-        running_loss = 0.0
-        print(len(trainloader))
-        for i, data in enumerate(trainloader, 0):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data[0].to(device), data[1].to(device)
+    # for epoch in range(20):  # loop over the dataset multiple times
+    #     print(f"Start training epoch {epoch}")
+    #     running_loss = 0.0
+    #     print(len(trainloader))
+    #     for i, data in enumerate(trainloader, 0):
+    #         # get the inputs; data is a list of [inputs, labels]
+    #         inputs, labels = data[0].to(device), data[1].to(device)
 
-            # zero the parameter gradients
-            optimizer.zero_grad()
+    #         # zero the parameter gradients
+    #         optimizer.zero_grad()
 
-            # forward + backward + optimize
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+    #         # forward + backward + optimize
+    #         outputs = model(inputs)
+    #         loss = criterion(outputs, labels)
+    #         loss.backward()
+    #         optimizer.step()
 
-            # print statistics
-            running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                    (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
+    #         # print statistics
+    #         running_loss += loss.item()
+    #         if i % 2000 == 1999:    # print every 2000 mini-batches
+    #             print('[%d, %5d] loss: %.3f' %
+    #                 (epoch + 1, i + 1, running_loss / 2000))
+    #             running_loss = 0.0
             
-            correct = 0
-            total = 0
-        with torch.no_grad():
-            for data in testloader:
-                images, labels = data[0].to(device), data[1].to(device)
-                outputs = model(images)
-                _, predicted = torch.max(outputs.data, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
+    #         correct = 0
+    #         total = 0
+    #     with torch.no_grad():
+    #         for data in testloader:
+    #             images, labels = data[0].to(device), data[1].to(device)
+    #             outputs = model(images)
+    #             _, predicted = torch.max(outputs.data, 1)
+    #             total += labels.size(0)
+    #             correct += (predicted == labels).sum().item()
 
-        print('Accuracy of the network on the 10000 test images: %d %%' % (
-            100 * correct / total))
+    #     print('Accuracy of the network on the 10000 test images: %d %%' % (
+    #         100 * correct / total))
 
 
 
-    print('Finished Training')
-    PATH = './cifar_net.pth'
-    torch.save(model.state_dict(), PATH)
+    # print('Finished Training')
+    # PATH = './cifar_net.pth'
+    # torch.save(model.state_dict(), PATH)
 
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for data in testloader:
-            images, labels = data[0].to(device), data[1].to(device)
-            outputs = model(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+    # correct = 0
+    # total = 0
+    # with torch.no_grad():
+    #     for data in testloader:
+    #         images, labels = data[0].to(device), data[1].to(device)
+    #         outputs = model(images)
+    #         _, predicted = torch.max(outputs.data, 1)
+    #         total += labels.size(0)
+    #         correct += (predicted == labels).sum().item()
 
-    print('Accuracy of the network on the 10000 test images: %d %%' % (
-        100 * correct / total))
+    # print('Accuracy of the network on the 10000 test images: %d %%' % (
+    #     100 * correct / total))
 
 
 if __name__ == "__main__":
