ea3da85c7b3b45266ab18a49e82966290a3dded0
Xiangfeng(Allen) Zhu <xzhu27@ucsc.edu>
2020-08-30 15:30:20 -0400

    Update cifar10

--- cifar.py
+++ cifar.py
@@ -33,77 +33,68 @@ def main():
     classes = ('plane', 'car', 'bird', 'cat',
             'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
    
-    if args.baseline:
-        model = resnet20().to(device)
-    else:
-        model = se_resnet20(num_classes=10, reduction=args.reduction).to(device)
+    # if args.baseline:
+    model_baseline = resnet20().to(device)
+    # else:
+    model = se_resnet20(num_classes=10, reduction=args.reduction).to(device)
 
-    model.load_state_dict(torch.load('checkpoint/cifar_senet.pth'))
-    # for name, value in model.named_parameters():
-    #     # value.requires_grad = False
-    #     print('name: {0},\t grad: {1}'.format(name, value.requires_grad))
-    torch.save(model, 'checkpoint/cifar_senet_full.pth')
+    model_baseline.load_state_dict(torch.load('checkpoint/cifar_baseline.pth'))
+    for name, value in model.named_parameters():
+        if name in model_baseline.state_dict():
+            value.data = model_baseline.state_dict()[name].detach().clone()
+            value.requires_grad = False
 
+    test(model, testloader)
 
-    # criterion = nn.CrossEntropyLoss()
-    # optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=0.001, momentum=0.9)
 
-    # for epoch in range(20):  # loop over the dataset multiple times
-    #     print(f"Start training epoch {epoch}")
-    #     running_loss = 0.0
-    #     print(len(trainloader))
-    #     for i, data in enumerate(trainloader, 0):
-    #         # get the inputs; data is a list of [inputs, labels]
-    #         inputs, labels = data[0].to(device), data[1].to(device)
+    for epoch in range(20):  # loop over the dataset multiple times
+        print(f"Start training epoch {epoch}")
+        running_loss = 0.0
+        print(len(trainloader))
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data[0].to(device), data[1].to(device)
 
-    #         # zero the parameter gradients
-    #         optimizer.zero_grad()
+            # zero the parameter gradients
+            optimizer.zero_grad()
 
-    #         # forward + backward + optimize
-    #         outputs = model(inputs)
-    #         loss = criterion(outputs, labels)
-    #         loss.backward()
-    #         optimizer.step()
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-    #         # print statistics
-    #         running_loss += loss.item()
-    #         if i % 2000 == 1999:    # print every 2000 mini-batches
-    #             print('[%d, %5d] loss: %.3f' %
-    #                 (epoch + 1, i + 1, running_loss / 2000))
-    #             running_loss = 0.0
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                    (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
             
-    #         correct = 0
-    #         total = 0
-    #     with torch.no_grad():
-    #         for data in testloader:
-    #             images, labels = data[0].to(device), data[1].to(device)
-    #             outputs = model(images)
-    #             _, predicted = torch.max(outputs.data, 1)
-    #             total += labels.size(0)
-    #             correct += (predicted == labels).sum().item()
-
-    #     print('Accuracy of the network on the 10000 test images: %d %%' % (
-    #         100 * correct / total))
+        test(model, testloader)
 
 
 
-    # print('Finished Training')
-    # PATH = './cifar_net.pth'
-    # torch.save(model.state_dict(), PATH)
+    print('Finished Training')
+    PATH = './cifar_transfer.pth'
+    torch.save(model.state_dict(), PATH)
 
-    # correct = 0
-    # total = 0
-    # with torch.no_grad():
-    #     for data in testloader:
-    #         images, labels = data[0].to(device), data[1].to(device)
-    #         outputs = model(images)
-    #         _, predicted = torch.max(outputs.data, 1)
-    #         total += labels.size(0)
-    #         correct += (predicted == labels).sum().item()
 
-    # print('Accuracy of the network on the 10000 test images: %d %%' % (
-    #     100 * correct / total))
+def test(model, testloader):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data[0].to(device), data[1].to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
 
+    print('Accuracy of the network on the 10000 test images: %d %%' % (
+        100 * correct / total))
 
 if __name__ == "__main__":
     import argparse
